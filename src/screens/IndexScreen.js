cb79d7743c16290e1096bc9955c91bf253f6f404
parth-v <parthvashisht20@gmail.com>
2020-05-25 18:15:37 +0530

    completed add blog functionality

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,25 @@
+import React,{ useContext } from 'react';
+import { Text, View, StyleSheet, FlatList, Button } from 'react-native';
+import BlogContext from '../context/BlogContext';
+
+const IndexScreen = () => {
+	const { data, addBlogPost } = useContext(BlogContext);
+
+	return (
+		<View>
+			<Text>Index Screen</Text>
+			<Button title="Add post" onPress={addBlogPost} />
+			<FlatList
+				data={data}
+				keyExtractor={blog => blog.title}
+				renderItem={({ item }) => {
+					return <Text>{item.title}</Text>;
+				}}
+			/>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
