99a1be8d6de0dd6003bf36235e68e92344eee99d
parth-v <parthvashisht20@gmail.com>
2020-05-26 00:35:39 +0530

    addition and deletion of blog finished

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,25 +1,46 @@
 import React,{ useContext } from 'react';
-import { Text, View, StyleSheet, FlatList, Button } from 'react-native';
+import { Text, View, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
+import { Fontisto } from '@expo/vector-icons';
 
 const IndexScreen = () => {
-	const { state, addBlogPost } = useContext(Context);
+	const { state, addBlogPost, deleteBlogPost } = useContext(Context);
 
 	return (
 		<View>
-			<Text>Index Screen</Text>
 			<Button title="Add post" onPress={addBlogPost} />
 			<FlatList
 				data={state}
 				keyExtractor={blog => blog.title}
 				renderItem={({ item }) => {
-					return <Text>{item.title}</Text>;
+					return (
+						<View style={styles.blogListContainer} >
+							<Text style={styles.blogText}>{item.title} - {item.id}</Text>
+							<TouchableOpacity onPress={() => deleteBlogPost(item.id)} >
+								<Fontisto style={styles.icon} name="trash"/>
+							</TouchableOpacity>
+						</View>
+					);
 				}}
 			/>
 		</View>
 	);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	blogListContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		padding: 10,
+		borderTopWidth: 2,
+		borderColor: 'gray'
+	},
+	icon: {
+		fontSize: 25
+	},
+	blogText: {
+		fontSize: 18
+	}
+});
 
 export default IndexScreen;
